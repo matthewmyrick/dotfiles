3770530685c0a0783fae155121f1f8c099e28cda
matthewmyrick <matthewmyrick2@gmail.com>
2025-08-20 21:25:40 -0400

    Update

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,37 @@
 return {
-    { "catppuccin/nvim", name = "catppuccin", lazy = false },
-    {
-        "LazyVim/LazyVim",
-        opts = {
-            colorscheme = "catppuccin",
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      integrations = {
+        bufferline = true,
+        cmp = true,
+        gitsigns = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        lsp_trouble = true,
+        mason = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
         },
+        navic = { enabled = true, custom_bg = "lualine" },
+        snacks = true,
+        noice = true,
+        notify = true,
+        semantic_tokens = true,
+        telescope = true,
+        treesitter = true,
+        which_key = true,
+      },
     },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
 }
---
+++ nvim/lua/plugins/ui.lua
@@ -0,0 +1,4 @@
+return {
+  -- Disable LazyVim's default bufferline to avoid conflicts
+  { "akinsho/bufferline.nvim", enabled = false },
+}
