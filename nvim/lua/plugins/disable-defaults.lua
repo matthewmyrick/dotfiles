bf25a9bdbb884f60dae1b1420118299acd0144fa
Matthew Myrick <matthewmyrick2@gmail.com>
2025-09-02 13:45:51 -0400

    Update

--- nvim/lua/plugins/disable-defaults.lua
+++ nvim/lua/plugins/disable-defaults.lua
@@ -1,5 +1,8 @@
 -- Disable LazyVim default plugins that we want to customize
 return {
-  -- Note: We don't disable bufferline here anymore since it conflicts with our custom one
-  -- Instead we override LazyVim's configuration in ui.lua
+  -- Disable LazyVim's bufferline integration to prevent conflicts
+  {
+    "akinsho/bufferline.nvim",
+    enabled = false,
+  },
 }
