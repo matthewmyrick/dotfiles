8fd0deee1d2408b43bbaa8302cd1c7169b768bde
Matthew Myrick <matthewmyrick2@gmail.com>
2025-09-08 15:19:07 -0400

    Update

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,15 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Auto-format terragrunt.hcl files on save
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = "terragrunt.hcl",
+  callback = function()
+    local file = vim.fn.expand("%:p")
+    vim.cmd("silent !terragrunt hclfmt " .. file)
+    vim.cmd("edit!")
+  end,
+  group = vim.api.nvim_create_augroup("TerragruntFormat", { clear = true }),
+  desc = "Format terragrunt.hcl files on save",
+})
