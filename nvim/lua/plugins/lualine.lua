3d5ca55819d51f4bd797d791c73a6ee7f94e8e50
Matthew Myrick <matthewmyrick2@gmail.com>
2025-09-15 22:13:27 -0400

    Update

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,21 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  opts = function(_, opts)
+    -- Override the default time format in the status line
+    if not opts.sections then
+      opts.sections = {}
+    end
+    if not opts.sections.lualine_z then
+      opts.sections.lualine_z = {}
+    end
+    
+    -- Add custom time format to the right side of the status line
+    table.insert(opts.sections.lualine_z, {
+      function()
+        return os.date("%I:%M %p EST")
+      end,
+    })
+    
+    return opts
+  end,
+}
