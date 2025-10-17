3cb11ac5f8832b61813775cc90a2ebe0b59644db
Matthew Myrick <matthewmyrick2@gmail.com>
2025-10-17 09:40:45 -0400

    remove old nvim

--- nvim-old/init.lua
+++
@@ -1,5 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
--- Window focus highlighting disabled to prevent pane highlighting
--- require("config.window-focus").setup()
