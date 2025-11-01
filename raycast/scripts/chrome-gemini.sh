0a0983d462ddd5a222efe43dc314f0077982436e
matthewmyrick <matthewmyrick2@gmail.com>
2025-11-01 17:58:38 -0400

    Update

--- raycast/scripts/chrome-gemini.sh
+++ raycast/scripts/chrome-gemini.sh
@@ -15,23 +15,22 @@
 # @raycast.author matthew_myrick
 # @raycast.authorURL https://raycast.com/matthew_myrick
 
-# Check if chrome-cli is installed
 if ! command -v chrome-cli &> /dev/null; then
     echo "chrome-cli is not installed. Run: brew install chrome-cli"
     exit 1
 fi
 
-# Find the first tab with "gemini.google.com" in the URL
-# grep -m 1 stops after the first match
-# gemini_tab_line=$(OUTPUT_FORMAT=json chrome-cli list tabs | jq -r ".tabs[].url")
+if ! command -v jq &> /dev/null; then
+  echo "jq is not installed. Run: brew install jq"
+  exit 1
+fi
+
 gemini_tab_id=$(OUTPUT_FORMAT=json chrome-cli list tabs | jq -r '.tabs[] | select(.url | contains("gemini.google.com")) | .id' | head -n 1)
 
 if [ -n "$gemini_tab_id" ]; then
-    # FOUND: Activate the tab
-    echo "found $gemini_tab_id"
+    echo "Found an existing Gemini Tab"
     chrome-cli activate -t "$gemini_tab_id" --focus
 else
-    # NOT FOUND: Open a new tab
-    echo "not found"
+    echo "No existing Gemini Tabs, loading a new one"
     chrome-cli open "https://gemini.google.com/"
 fi
