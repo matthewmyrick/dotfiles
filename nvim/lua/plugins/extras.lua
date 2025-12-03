d8b69213421b5ee66989e3800dcad0b5e3f41006
Matthew Myrick <matthewmyrick@ip-192-168-1-97.us-east-2.compute.internal>
2025-12-03 14:41:45 -0500

    Update

--- nvim/lua/plugins/extras.lua
+++ nvim/lua/plugins/extras.lua
@@ -9,4 +9,40 @@ return {
     end,
   },
 
+  -- Search and replace
+  {
+    "MagicDuck/grug-far.nvim",
+    cmd = "GrugFar",
+    keys = {
+      {
+        "<leader>sr",
+        function()
+          require("grug-far").open({ prefills = { paths = vim.fn.expand("%") } })
+        end,
+        desc = "Search and Replace (current file)",
+      },
+      {
+        "<leader>sw",
+        function()
+          require("grug-far").open({ prefills = { search = vim.fn.expand("<cword>") } })
+        end,
+        desc = "Search word under cursor",
+      },
+      {
+        "<leader>sw",
+        mode = "v",
+        function()
+          require("grug-far").with_visual_selection()
+        end,
+        desc = "Search visual selection",
+      },
+    },
+    config = function()
+      require("grug-far").setup({
+        headerMaxWidth = 80,
+        windowCreationCommand = "vsplit",
+      })
+    end,
+  },
+
 }
