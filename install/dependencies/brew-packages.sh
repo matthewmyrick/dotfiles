6919c173c70cf8e821040366b0517dd0bb41a4a8
matthewmyrick <matthewmyrick2@gmail.com>
2026-01-29 14:29:24 -0500

    update

--- install/dependencies/brew-packages.sh
+++ install/dependencies/brew-packages.sh
@@ -18,7 +18,7 @@ BREW_PACKAGES=(
   grc nmap iproute2mac hyperfine gdu dua-cli
   xplr shpotify wireshark imagemagick otree
   xo/xo/usql chrome-cli terminal-notifier
-  lazysql yq tf-summarize
+  lazysql yq tf-summarize ical-buddy
 )
 
 
